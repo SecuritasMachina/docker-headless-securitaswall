8c0bcfbf9273f49e6c486026547db6ce78b5051f
Your Name <jaxtrx>
2020-02-11 00:35:54 -0500

    Remove extras,

--- src/template/homeDir/bin/ctlTomcat.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-chmod ug+x $HOME/apps/tomcat/bin/catalina.sh
-$HOME/apps/tomcat/bin/catalina.sh $1
--- src/template/homeDir/bin/eclipse.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-chmod u+x $HOME/apps/eclipse/eclipse 
-$HOME/apps/eclipse/eclipse -data $HOME/data/eclipseWorkspace >nul &
